c7733b9c98a5e7bfc9fc2175ff14ff3d67106b17
Nicky Harpor <88788515+nickyharpor@users.noreply.github.com>
2025-12-23 20:46:16 +0000

    Update config_test.py

--- bot/config_test.py
+++ bot/config_test.py
@@ -1,7 +1,7 @@
 # telegram
-api_id = 0  # CHANGE_THIS
-api_hash = ''  # CHANGE_THIS
-bot_token = ''  # CHANGE_THIS
+api_id = 0  # you must change this to api_id received from my.telegram.org
+api_hash = ''  # you must change this to api_hash received from my.telegram.org
+bot_token = ''  # you must change this to bot_token received from @BotFather
 session_name = 'ajna_guru_test'
 proxy = False
 proxy_protocol = 'socks5'
